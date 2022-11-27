bb760c79c9af67f65400986f16409b421436869c
miemie <miemie@192.168.0.102>
2022-11-27 16:00:04 +0800

    create a client

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,11 +9,7 @@ namespace Persistence
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base (options)
-        {
-
-        }
-
+        public DataContext(DbContextOptions options) : base (options){}
         public DbSet<Activity> Activities { get; set; }
     }
 
